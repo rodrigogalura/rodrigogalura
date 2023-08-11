21c4e4daf1edb82ec5eb0814c143a8605005c5be
rodrigogalura <rodrigogalura3rd@gmail.com>
2023-08-11 21:17:53 +0800

    Add Loading indicator on images of modal

--- src/assets/js/portfolio-data.js
+++ src/assets/js/portfolio-data.js
@@ -1,14 +1,14 @@
 const lexus = {
     name: 'Lexus Courseware',
     category: 'Website',
-    preview: { src: '/img/portfolio/lexus/preview.png', alt: "Lexus Preview" },
+    preview: { src: '/img/portfolio/lexus/preview.png', alt: 'Lexus Preview' },
     screenshots: [
-        { src: '/img/portfolio/lexus/grade11.png', alt: 'Grade 11' },
-        { src: '/img/portfolio/lexus/grade12.png', alt: 'Grade 12' },
-        { src: '/img/portfolio/lexus/tech-voc.png', alt: 'Technology Vocational' },
-        { src: '/img/portfolio/lexus/downloads.png', alt: 'Downloads' },
-        { src: '/img/portfolio/lexus/about-us.png', alt: 'About Us' },
-        { src: '/img/portfolio/lexus/contact-us.png', alt: 'Contact Us' }
+        { src: '/img/portfolio/lexus/grade11.png', alt: 'Grade 11', isLoaded: false },
+        { src: '/img/portfolio/lexus/grade12.png', alt: 'Grade 12', isLoaded: false },
+        { src: '/img/portfolio/lexus/tech-voc.png', alt: 'Technology Vocational', isLoaded: false },
+        { src: '/img/portfolio/lexus/downloads.png', alt: 'Downloads', isLoaded: false },
+        { src: '/img/portfolio/lexus/about-us.png', alt: 'About Us', isLoaded: false },
+        { src: '/img/portfolio/lexus/contact-us.png', alt: 'Contact Us', isLoaded: false }
     ]
 }
 
--- src/components/PortfolioModal.vue
+++ src/components/PortfolioModal.vue
@@ -15,13 +15,22 @@ const emit = defineEmits(['closeModal'])
                 <h4 class="h3 modal-title" v-text="title"></h4>
                 <p class="mb-5" v-text="category"></p>
                 <div class="grid lg:grid-cols-3 gap-5">
-                    <img
-                        v-for="ss in screenshots"
-                        :key="ss.alt"
-                        :src="ss.src"
-                        class="rounded-lg w-full bg-yellow-500 p-1"
-                        :alt="ss.alt"
-                    />
+                    <div class="relative" v-for="ss in screenshots" :key="ss.alt">
+                        <img
+                            :src="ss.src"
+                            class="rounded-lg w-full h-full bg-yellow-500 p-1"
+                            :alt="ss.alt"
+                            loading="lazy"
+                            @load="ss.isLoaded = true"
+                        />
+                        <p
+                            v-if="!ss.isLoaded"
+                            class="animate-pulse text-gray-500 text-1xl absolute left-1/2 top-1/2"
+                            style="transform: translate(-50%, -50%)"
+                        >
+                            Loading...
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
