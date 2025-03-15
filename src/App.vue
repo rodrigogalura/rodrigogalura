8b06c1149eefcb038854ca3b2cd131cf8fa6bb8b
Rodrigo Galura <rodrigogalura3rd@gmail.com>
2025-03-15 17:29:22 +0800

    dynamic last updated

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import SideBar from './components/SideBar.vue'
 import NavBar from './components/NavBar.vue'
+
+const last_updated = import.meta.env.VITE_UPDATED
 </script>
 
 <template>
-    <div class="info-content" style="display: flex; justify-content: center">
-        <p class="title">Last Updated Date: August 2023</p>
+    <div class="info-content" style="display: flex; justify-content: center" v-if="last_updated">
+        <p class="title">Last Updated Date: {{ last_updated }}</p>
     </div>
 
     <main>
